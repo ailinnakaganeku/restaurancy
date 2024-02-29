13c2e1b18ed0b7dae6be1df9c471e22a44055cff
ailinnakaganeku <ailin.nakaganeku@gmail.com>
2024-02-28 23:51:20 -0300

    chore(RESTAURANCY-3): Adding Button Styles

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    ppr: true,
      serverActions: {
       allowedOrigins: ["my-proxy.com", "*.my-proxy.com"],
     },
