ff3928181c706a048c4f2642dd56ecba8add3c22
ailinnakaganeku <ailin.nakaganeku@gmail.com>
2024-02-28 22:16:59 -0300

    fix(RESTAURANCY-1): fix error while building

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    ppr: true,
     serverActions: {
       allowedOrigins: ["my-proxy.com", "*.my-proxy.com"],
     },
